d6d41fbdfccc8d00333f59471142f42741564822
takanishi <kazukazu1130@icloud.com>
2023-11-28 21:50:18 +0900

    変更（2日目）

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocalMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
 
