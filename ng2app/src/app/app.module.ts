bb161ae18043e4de4206c5c706f5549ab2b089c8
takanishi <kazukazu1130@icloud.com>
2023-11-29 19:17:18 +0900

    router-outletが反映されず、一時中断

---
+++ ng2app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoService } from './todo.service';
+import { AppRoutingModule } from './app-routing.module'; // AppRoutingModule のインポート
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+    // 他のコンポーネント
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule, // AppRoutingModule を追加
+    // 他の必要なモジュール
+  ],
+  providers: [TodoService],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
