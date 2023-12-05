372cce503e0e919a432dfd0deeef26c72d493dfd
takanishi <kazukazu1130@icloud.com>
2023-12-06 00:17:17 +0900

    モーダルコンポーネント作成

---
+++ ng2app/src/app/todo-list/todo-list-card/todo-list-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-list-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo-list-card.component.html',
+  styleUrl: './todo-list-card.component.css'
+})
+export class TodoListCardComponent {
+
+}
